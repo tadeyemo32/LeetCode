27e96c0227ab04bb69739ea6ec80d85fbca0c06d
tadeyemo32 <tadeyemo32@gmail.com>
2025-07-20 19:43:49 +0800

    new problem

--- 704. Binary Search/main.cpp
+++ 704. Binary Search/main.cpp
@@ -3,33 +3,35 @@
 using namespace std; 
 
 
-
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
 
 	int low = 0; 
-	int high = nums.size(); 
-	int mid =(low+high)/2;
+	int high = nums.size()-1; 
+	
 	
 
 	while (low<=high) {
+    int mid =(low+high)/2;
 	    if(nums[mid] == target){
 		
 		return mid;
 	    }
 
 	    if(nums[mid]< target){
-		low = nums[mid];
+		low = mid+1;
 	    }
 
-	    if(){
-
+	    if(nums[mid]>target){
+            high = mid-1;
 	    }
 	
 	}
+    return -1;
     }
 };
+
 
 
 
