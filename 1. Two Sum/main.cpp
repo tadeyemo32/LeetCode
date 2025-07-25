c6a422ae00b48fa945cdddfe95d94c61ad526b76
tadeyemo32 <tadeyemo32@gmail.com>
2025-07-25 20:10:08 +0800

    new problem

--- 1. Two Sum/main.cpp
+++ 1. Two Sum/main.cpp
@@ -1,17 +1,27 @@
-#include <iostream> 
+#include "../../bits/stdc++.h"
+using namespace std;
 
+class Solution
+{
+public:
+  vector<int> twoSum(vector<int> &nums, int target)
+  {
+    unordered_map<int, int> vals;
+    for (int i = 0; i < nums.size(); i++)
+    {
+      int complement = target - nums[i];
+      if (vals.find(complement) != vals.end())
+      {
+        return {vals[complement], i};
+      }
+      vals[nums[i]] = i;
+    }
+    return {-1};
+  }
+};
 
+int main()
+{
 
-using namespace std ; 
-
-
-;
-
-
-int main(){
-
-
-
-
-return 0;
+  return 0;
 }
