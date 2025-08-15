3af13c3ceba806e9303cb6a66fb4145fdbee20d0
tadeyemo32 <tadeyemo32@gmail.com>
2025-08-15 17:20:09 +0800

    new problem

---
+++ 219 Contains Duplicate II/main.cpp
@@ -0,0 +1,43 @@
+#include "../../bits/stdc++.h"
+
+
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int,int> mp; 
+
+
+        for(int i =0;i<nums.size();i++){
+            
+           if (mp.find(nums[i]) != mp.end() && i - mp[nums[i]] <= k) {
+                return true ;
+            }
+            mp[nums[i]] =i ;
+
+        }
+        return false; 
+    }
+};
+
+
+
+
+
+
+int main(){
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
